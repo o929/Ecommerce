7f3d1fc794f40de9f2d9683709b61b78dac1c7b4
Omar Yagoub <omaryagoub96@gmail.com>
2025-09-26 12:48:48 +0200

    Updates

--- assets/chevron-right-Dcal0fDZ.js
+++ assets/chevron-right-Dcal0fDZ.js
@@ -1,4 +1,4 @@
-import{a as o}from"./index-CQBa0asV.js";/**
+import{a as o}from"./index-OPZrWuSV.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
