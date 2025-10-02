265c6d9a59b8f300c43afd0fcbde681e9eed6fff
Omar Yagoub <omaryagoub96@gmail.com>
2025-10-02 11:18:49 +0200

    Updates

--- assets/chevron-right-BnjFddC9.js
+++ assets/chevron-right-BnjFddC9.js
@@ -1,4 +1,4 @@
-import{a as o}from"./index-B144rz7j.js";/**
+import{a as o}from"./index-JWOhfUiA.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
