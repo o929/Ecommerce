9af8e6b66899b9223058bf444f6747f0b6b235a3
Omar Yagoub <omaryagoub96@gmail.com>
2025-10-02 10:58:25 +0200

    Updates

--- assets/lucide-react-BVLgE613.js
+++ assets/lucide-react-BVLgE613.js
@@ -1,4 +1,4 @@
-import{a as e,A as a,C as c,b as t,e as o,E as d,H as n,f as h,h as s,L as i,M as y,i as r,j as k,P as p,k as l,l as u,S as M,T as v,m,U as _,W as g,X as x}from"./index-qdjItk4H.js";import{I as mX}from"./index-qdjItk4H.js";import{C as L,a as w}from"./chevron-right-BkJehp5H.js";/**
+import{a as e,A as a,C as c,b as t,e as o,E as d,H as n,f as h,h as s,L as i,M as y,i as r,j as k,P as p,k as l,l as u,S as M,T as v,m,U as _,W as g,X as x}from"./index-BkzZ_bPc.js";import{I as mX}from"./index-BkzZ_bPc.js";import{C as L,a as w}from"./chevron-right-BEQs9OCv.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
