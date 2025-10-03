2473d370c82ff4754bdd4b13f13467f93cfb0e5f
Omar Yagoub <omaryagoub96@gmail.com>
2025-10-03 11:25:58 +0200

    Updates

--- assets/chevron-right-CmfQuPyX.js
+++ assets/chevron-right-CmfQuPyX.js
@@ -1,4 +1,4 @@
-import{a as o}from"./index-JWOhfUiA.js";/**
+import{a as o}from"./index-BkAYhzfB.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
